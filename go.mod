29158eb7cc5571e324fdae33d3ecb29d8de93585
engkos ragda <engkos.ragda@gmail.com>
2023-04-21 16:55:37 +0700

    defendecy new

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/engkoskostaman97/app-say-hello
+
+go 1.19
+
+require github.com/engkoskostaman97/go-say-hello v1.0.0 
---
+++ main.go
@@ -0,0 +1,7 @@
+package app_say_hello
+
+import go_say_hello "github.com/engkoskostaman97/go-say-hello"
+
+func main() {
+	go_say_hello.SayHello()
+}
